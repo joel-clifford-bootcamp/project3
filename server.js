2bc6001d9c155f1f20f91f5c27f3b923477c5bde
Joel Clifford <joel.clifford@icloud.com>
2020-06-27 20:38:14 -0400

    added bixi-bikes data refresh cron-job every day at 11:59pm

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var session = require("express-session");
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
 
+const dataRefreshCron = require("./utils/cron/index");
+
 var refreshBixiStations = require('./utils/import/bixiStations'); 
 
 // Setting up port and requiring models for syncing
@@ -31,14 +33,19 @@ app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true 
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Regularly update data from external APIs 
+dataRefreshCron();
+
 // Requiring our routes
 // require("./routes/html-routes.js")(app);
 // require("./routes/api/index.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
-
+  
+  // Call external APIs to populate data
   refreshBixiStations();
+
   // Start the API server
   app.listen(PORT, function() {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
