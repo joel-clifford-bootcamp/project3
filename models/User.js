43aa52c505015a0cd21cee08ccbf145750090406
Joel Clifford <joel.clifford@icloud.com>
2020-06-27 20:11:02 -0400

    added restored changed lat/lng data tyes to double and added setter

--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 // Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
 const bcrypt = require("bcryptjs");
-const StationComment = require("./StationComment");
 
 // Creating our User model
 module.exports = function(sequelize, DataTypes) {
