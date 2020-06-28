f4f7f6d42e6d0a5f7aaddf98c37e210c8a7ff13d
Joel Clifford <joel.clifford@icloud.com>
2020-06-27 22:52:21 -0400

    added route for realtime bixi data

--- routes/api/BixiStation.js
+++ routes/api/BixiStation.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const db = require('../../models');
-const { sequelize } = require('../../models');
+const getRealTimeData = require('../../utils/api/StationsData');
 
 const router = express.Router();
 
@@ -22,12 +22,12 @@ const router = express.Router();
 // Return the closest 10 BixiStations, based on searched location
 // Note: coord_diff correlates to distance well enough - Google used on
 // front end to calcualte actual path distance for each plotted object
-router.get('/api/bixi/', (req, res) => {
+router.get('/bixi/', (req, res) => {
 
     db.BixiStation.findAll({
-        attributes:['id', 'name', 'lat', 'lng', 'capacity', 
+        attributes:['id', 'idx', 'name', 'lat', 'lng', 'capacity', 
             [db.sequelize.literal(`ABS(lat - ${req.query.lat}) + ABS(lng - ${req.query.lng})`), 'coord_diff']],
-        order: sequelize.col('coord_diff'),
+        order: db.sequelize.col('coord_diff'),
         limit: 10
     })
     .then(data => {
@@ -36,6 +36,24 @@ router.get('/api/bixi/', (req, res) => {
     .catch(err => {
         res.status(400).end();
     })
+});
+
+// Return real time station data for stations identified by idx
+router.get('/bixi/realtime', (req, res) => {
+        getRealTimeData((data) => {
+            const stationsArray = [];
+
+            try{            
+                req.body.idxArray.forEach(idx => {
+                    stationsArray.push(data[idx]);
+                });
+
+                res.status(200).send(stationsArray);
+            }
+            catch{
+                res.status(400).end();
+            }
+        });
 });
 
 module.exports = router;
