84d398b3ccc2c063cabe79b9ddac40d314797a3e
Brice Boutet <boutetlb@gmail.com>
2020-06-30 20:30:05 -0400

    Nav bar added into Map component

--- client/src/components/MapContainer/index.js
+++ client/src/components/MapContainer/index.js
@@ -1,22 +1,15 @@
-import React, { useState, useEffect, Component } from 'react';
-import { LoadScript } from '@react-google-maps/api';
-import Map from '../Map'
-
-
-
-
+import React, { useState, useEffect, Component } from "react";
+import { LoadScript } from "@react-google-maps/api";
+import Map from "../Map";
+import Nav from "../Nav";
 function MapContainer() {
-  
   return (
-    <LoadScript
-      googleMapsApiKey=''
-    >
-      <Map />
-    </LoadScript>
-  )
-    
+    <div>
+      <Nav />
+      <LoadScript googleMapsApiKey="AIzaSyDE2yBUEZx3Cup_pwq22o_WferVgBpgSdE">
+        <Map />
+      </LoadScript>
+    </div>
+  );
 }
-
-
-  export default MapContainer
-
+export default MapContainer;
