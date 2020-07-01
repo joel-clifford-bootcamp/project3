2fae90eef85be7f394d29ccf9dd476217745c096
Massimo Guida <massimo@massimoguida.com>
2020-06-30 20:33:19 -0400

    added links in singup and signin pages, created css files for pages, styled profile page, and created new nav for landing pages

--- client/src/components/MapContainer/index.js
+++ client/src/components/MapContainer/index.js
@@ -1,22 +1,17 @@
-// import React, { useState, useEffect, Component } from 'react';
-// import { LoadScript } from '@react-google-maps/api';
-// import Map from '../Map'
+import React, { useState, useEffect, Component } from "react";
+import { LoadScript } from "@react-google-maps/api";
+import Map from "../Map";
+import Nav from "../Nav";
 
+function MapContainer() {
+  return (
+    <div>
+      <Nav />
+      <LoadScript googleMapsApiKey="AIzaSyDE2yBUEZx3Cup_pwq22o_WferVgBpgSdE">
+        <Map />
+      </LoadScript>
+    </div>
+  );
+}
 
-
-
-// function MapContainer() {
-  
-//   return (
-//     // <LoadScript
-//     //   googleMapsApiKey='AIzaSyDE2yBUEZx3Cup_pwq22o_WferVgBpgSdE'
-//     // >
-//     //   <Map />
-//     // </LoadScript>
-//   )
-    
-// }
-
-
-//   export default MapContainer
-
+export default MapContainer;
