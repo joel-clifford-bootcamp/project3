12375ad1bddc0cab0019aca45a43696d442e1e20
Massimo Guida <massimo@massimoguida.com>
2020-07-04 18:46:11 -0400

    added styled and functional link buttons to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,18 @@
 import React from "react";
-import Nav from "../components/Nav"
+import Nav from "../components/Nav";
+import { Link } from "react-router-dom";
 import "../home.css";
 
 function Home() {
   return (
     <div>
-        <Nav />
-        <h1>Find Your <span>Perfect Place.</span></h1>
-        <div className="row mood-button-row">
+      <Nav />
+      <div>
+        <h1>
+          Find Your <span>Perfect Place.</span>
+        </h1>
+      </div>
+      {/* <div className="row mood-button-row">
                     <div className="col s1"><button type="button" className="mood-buttons map"><img
                                 src="./assets/images/emoticons/Thankly-Face-1.png"
                                 className="mood-btn-img" /></button></div>
@@ -22,21 +27,88 @@ function Home() {
                                 className="mood-btn-img" /></button></div>
                 </div>
         <div className="row s12 m12 l12">
-            <div className="col s3">Map A Safe Route</div>
-            <div className="col s3">Park Your Bike</div>
-            <div className="col s3">Find a Bixi Bike</div>
-            <div className="col s3">Rate a Parking Spot</div>
-        </div>
-        <div className="row s12">
-            <div className="col s12 m12 l12">
-                <ul className="tabs s12">
-                    <li className="tab col s3"><a href="#test1">Routes</a></li>
+            <div className="col s3"><button className="button buttonHome"><i className="fas fa-map icon"></i></button><h5>Map A Safe Route</h5></div>
+            <div className="col s3"><button className="button buttonHome"><i className="fas fa-parking icon"></i></button>Park Your Bike</div>
+            <div className="col s3"><button className="button buttonHome"><i className="fas fa-map-marker-alt icon"></i></button>Find a Bixi Bike</div>
+            <div className="col s3"><button className="button buttonHome"><i className="fas fa-star icon"></i></button>Rate a Parking Spot</div> 
+        </div> */}
+      <div className="row s12">
+        <div className="col s12 m12 l12">
+          <ul className="tabs s12 m12">
+            <li className="tab col s3">
+              <button className="button buttonHome">
+              <a href="/map"><i className="fas fa-map icon"></i></a>
+              </button>
+              <a href="/map">Map A Safe Route</a>
+            </li>
+            <li className="tab col s3">
+              <button className="button buttonHome">
+              <a href="/map">
+                  <i className="fas fa-parking icon"></i>
+                  </a>
+              </button>
+              <a href="/map">Park Your Bike</a>
+            </li>
+            <li className="tab col s3">
+              <button className="button buttonHome">
+              <a href="/map">
+                  <i className="fas fa-map-marker-alt icon"></i>
+                </a>
+                </button>
+              <a className="active" href="/map">
+                Find a Bixi Bike
+              </a>
+            </li>
+            <li className="tab col s3 disabled">
+              <button className="button buttonHome">
+              <a href="/map">
+                  <i className="fas fa-star icon"></i>
+                </a>
+              </button>
+              <a 
+              href="/map"
+              className={
+                window.location.pathname === "/map"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+              >
+                  Rate a Parking Spot</a>
+            </li>
+            {/* <li className="tab col s3">
+              <button className="button buttonHome">
+                <i className="fas fa-map icon"></i>
+              </button>
+              <a href="#test1">Map A Safe Route</a>
+            </li>
+            <li className="tab col s3">
+              <button className="button buttonHome">
+                <i className="fas fa-parking icon"></i>
+              </button>
+              <a href="#test2">Park Your Bike</a>
+            </li>
+            <li className="tab col s3">
+              <button className="button buttonHome">
+                <i className="fas fa-map-marker-alt icon"></i>
+              </button>
+              <a className="active" href="#test3">
+                Find a Bixi Bike
+              </a>
+            </li>
+            <li className="tab col s3 disabled">
+              <button className="button buttonHome">
+                <i className="fas fa-star icon"></i>
+              </button>
+              <a href="#test4">Rate a Parking Spot</a>
+            </li> */}
+
+            {/* <li className="tab col s3"><a href="#test1">Routes</a></li>
                     <li className="tab col s3"><a href="#test3">Find a Parking Spot</a></li>
                     <li className="tab col s3"><a className="active" href="#test2">Find a Bixi Bike</a></li>
-                    <li className="tab col s3 disabled"><a href="#test4">Rate</a></li>
-                </ul>
-            </div>
+                    <li className="tab col s3 disabled"><a href="#test4">Rate</a></li> */}
+          </ul>
         </div>
+      </div>
     </div>
   );
 }
