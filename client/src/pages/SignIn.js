b2420bf7c087ac37da1bb9852adc6c27bb1e2059
Tobi Bailey <baileygtobi@gmail.com>
2020-07-07 17:15:30 -0400

    updated login and signup info

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,29 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "../signin.css";
 
-function SignIn() {
+const SignIn = () => {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+  
+    const handleSubmit = e => {
+      e.preventDefault();
+      console.log("email is " + email);
+      console.log("password is " + password);
+    };
+
   return (
-    <div class="row">
-        <div class="col s12 m6">
+    <div className="row">
+        <div className="col s12 m6">
             <div className="card signIn">
                 <h1>Welcome</h1>
                     <h6>New to Link-N-Park? <span className="signUpLink">Sign up</span> for free!</h6>
                         <div className="row">
-                            <form className="col s12">
+                            <form onSubmit={handleSubmit}>
                                 <div className="row">
                                     <div className="input-field col s12">
-                                        <input id="email" type="email" className="validate"/>
-                                            <label for="email">Email</label>
+                                        <input 
+                                        id="email" 
+                                        type="email" 
+                                        className="validate"
+                                        placeholder="Email Address"
+                                        name="email"
+                                        onChange={e => setEmail(e.target.value)}
+                                        />
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="input-field col s12">
-                                        <input id="password" type="password" className="validate"/>
-                                            <label for="password">Password</label>
+                                        <input 
+                                        id="password"
+                                        type="password" 
+                                        className="validate"
+                                        placeholder="Password"
+                                        name="password"
+                                        onChange={e => setPassword(e.target.value)}
+                                        />
                                     </div>
                                 </div>
+                            <button className="btn signInBtn" type="submit">Sign In</button>
                             </form>
-                            <a className="btn signInBtn">Sign In</a>
                         </div>
 
             </div>
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,45 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import "../signin.css";
 
-function SignUp() {
+
+const SignUp = () => {
+    const [firstName, setFirstName] = useState();
+    const [lastName, setLastName] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = e => {
+      e.preventDefault();
+      console.log("first name is " + firstName);
+      console.log("last name is " + lastName);
+      console.log("email is " + email);
+      console.log("password is " + password);
+    };
+
   return (
-    <div class="row">
-        <div class="col s12 m6">
+    <div className="row">
+        <div className="col s12 m1">
+            <img src="../assets/logo1.png" alt="Full White Logo"></img>
+        </div>
+        <div className="col s12 m6">
             <div className="card signIn">
                 <h1>Welcome</h1>
                 <h6>Already have an <span className="signUpLink">account?</span></h6>
                 <div className="row">
-                    <form className="col s12">
+                    <form onSubmit={handleSubmit}>
                         <div className="row">
                             <div className="input-field col s6">
-                                <input id="first_name" type="text" className="validate"/>
-                                <label for="first_name">First Name</label>
+                                <input 
+                                id="first_name" 
+                                type="text" 
+                                className="validate"
+                                placeholder="First Name"
+                                name="firstName"
+                                onChange={e => setFirstName(e.target.value)}
+                                />
                             </div>
                             <div className="input-field col s6">
-                                <input id="last_name" type="text" className="validate"/>
-                                <label for="last_name">Last Name</label>
+                                <input 
+                                id="last_name" 
+                                type="text" 
+                                className="validate"
+                                placeholder="Last Name"
+                                name="lastName"
+                                onChange={e => setLastName(e.target.value)}
+                                />
                             </div>
                         </div>
                         <div className="row">
                                 <div className="input-field col s12">
-                                    <input id="email" type="email" className="validate"/>
-                                        <label for="email">Email</label>
+                                    <input 
+                                    id="email" 
+                                    type="email" 
+                                    className="validate"
+                                    placeholder="Email Address"
+                                    name="email"
+                                    onChange={e => setEmail(e.target.value)}
+                                    />
                                 </div>
                             </div>
                             <div className="row">
-                                <div classNme="input-field col s12">
-                                    <input id="password" type="password" className="validate"/>
-                                        <label for="password">Password</label>
+                                <div className="input-field col s12">
+                                    <input 
+                                    id="password"
+                                    type="password" 
+                                    className="validate"
+                                    placeholder="Password"
+                                    name="password"
+                                    onChange={e => setPassword(e.target.value)}
+                                    />
                                 </div>
                             </div>
-                            <div className="row">
-                                <div classNme="input-field col s12">
-                                    <input id="confirmPassword" type="password" className="validate"/>
-                                        <label for="confirmPassword">Confirm Password</label>
+                            {/* <div className="row">
+                                <div className="input-field col s12">
+                                    <input 
+                                    id="confirmPassword" 
+                                    type="password" 
+                                    className="validate"
+                                    placeholder="Confirm Password"
+                                    name="confirmPassword"
+                                    onChange={e => setConfirmPassword(e.target.value)}
+                                    />
                                 </div>
-                            </div>
-                    </form>
-                    <a className="btn signInBtn">Sign Up</a>
+                            </div> */}
+                            <button className="btn signInBtn" type="submit">Sign Up</button>
+                        </form>
                 </div>
             </div>
         </div>
