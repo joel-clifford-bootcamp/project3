0545d9319fa2868dd4b542cd477645a1f9c5b09f
Joel Clifford <joel.clifford@icloud.com>
2020-07-07 21:06:08 -0400

    added bisiStations routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const userRoutes = require("./users");
+const bixiRoutes = require("./bixiStation")
 
 // User routes
 router.use("/users", userRoutes);
+router.use("/bixi", bixiRoutes);
 
 module.exports = router;
