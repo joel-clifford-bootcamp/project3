7ddf7218d9f2003b77adf23b50ca1aaf1ea199b0
Massimo Guida <massimo@massimoguida.com>
2020-07-09 20:57:11 -0400

    workign on comments

--- client/src/components/Comment/index.js
+++ client/src/components/Comment/index.js
@@ -1,11 +1,21 @@
 import "./style.css";
 import React, { Component } from "react";
-// import Remarkable from 'remarkable';
+import Remarkable from 'remarkable';
 // import RemarkableReactRenderer from 'remarkable-react';
 import M from "materialize-css";
 
+const data = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
+
 class Comments extends Component {
   
+  rawMarkup() {
+    let md = new Remarkable();
+    let rawMarkup = md.render(this.props.children.toString());
+    return { __html: rawMarkup };
+  }
 
   render() {
     return (
@@ -13,22 +23,26 @@ class Comments extends Component {
         <h2 className="commentAuthor">
           {this.props.author}
         </h2>
-        {this.props.children}
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
       </div>
     );
   }
 }
 
 class CommentList extends Component {
-  // rawMarkup() {
-  //   let md = new Remarkable();
-  //   let rawMarkup = md.render(this.props.children.toString());
-  //   return { __html: rawMarkup };
-  // }
 
   render() {
+
+    const commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+
     return (
       <div>
+          {commentNodes}
+
         <div className="commentList">
           <p>Yeahhhh I am a CommentList.</p>
         </div>
--- client/src/components/CommentBox/index.js
+++ client/src/components/CommentBox/index.js
@@ -4,12 +4,13 @@ import M from "materialize-css";
 import { Comments, CommentList, CommentForm } from "../Comment";
 
 
-function CommentBox() {
+function CommentBox(props) {
   return (
     <div className="commentBox">
       <h1>WOO My Comment Box</h1>
       <h2>We created a React div component! WOO!</h2>
-      <CommentList />
+      <CommentList data={this.props.data} />
+      {/* <CommentList /> */}
       <CommentForm />
     </div>
   );
