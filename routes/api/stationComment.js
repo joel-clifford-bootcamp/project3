824f4be22137deca53cf4d6bb79e39592497c6b9
Joel Clifford <joel.clifford@icloud.com>
2020-07-09 20:14:18 -0400

    added post route for new comment

--- routes/api/stationComment.js
+++ routes/api/stationComment.js
@@ -3,8 +3,12 @@ const commentsController = require("../../controllers/stationCommentsController"
 const db = require('../../models');
 
 
+router.route("/")
+    .post(commentsController.createComment);
+
 // Return 10 most recent StationComments for BixiStation with supplied id
 router.route('/:station_id')
-    .get(commentsController.getLatestComments);
+    .get(commentsController.getLatestComments)
+    
 
 module.exports = router;
