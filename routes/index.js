a389979a3cc44b0d6a23af21eaf90e1c18de046f
Massimo Guida <massimo.guidamusic@gmail.com>
2020-07-15 13:12:15 -0400

    Update index.js
    Fix deployment issue as GitHub was preventing me from resoving conflicts in previous commit.

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
+
   if (process.env.NODE_ENV === "production")
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
   else
