dbc700903960d79385cce7823368e56aeca16d70
Joel Clifford <joel.clifford@icloud.com>
2020-07-16 17:55:00 -0400

    return 401 error when no user logged inn

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -50,7 +50,7 @@ module.exports = {
   userData: function(req, res) {
     if (!req.user) {
         // The user is not logged in, send back an empty object
-        res.json({});
+        res.status(401);
       } else {
         // Otherwise send back the user's email and id
         // Sending back a password, even a hashed password, isn't a good idea
